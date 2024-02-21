f9a80d9d1d9b5731e435d890b435f16a15aac54b
Hmorgan22 <huntermorgan1219@yahoo.com>
2024-02-21 12:47:55 -0500

    added finish page and timer for quiz

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,11 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -14,6 +19,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -24,9 +31,13 @@ function reducer(state, action) {
     //if there is an error
     case "dataFailed":
       return { ...state, status: "error" };
-    //when start button is selected
+    //when start button is selected this will activate the quiz along with the timer that is based on the number of questions
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     //when an answer is selected during quiz
     case "newAnswer":
       //gets the current question
@@ -44,16 +55,41 @@ function reducer(state, action) {
     //triggers the next button once a question is answered
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    //triggers end of quiz screen and sets highscore
+    case "finished":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    //restarts quiz back to initial state
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+        highscore: state.highscore,
+      };
+    //causes the timer to tick down and end the quiz once the timer reaches 0
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
@@ -89,8 +125,24 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPoints={maxPoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
--- src/Components/NextButton.js
+++ src/Components/NextButton.js
@@ -1,14 +1,25 @@
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
 
-  return (
-    <button
-      onClick={() => dispatch({ type: "nextQuestion" })}
-      className="btn btn-ui"
-    >
-      Next Question
-    </button>
-  );
+  if (index < numQuestions - 1)
+    return (
+      <button
+        onClick={() => dispatch({ type: "nextQuestion" })}
+        className="btn btn-ui"
+      >
+        Next Question
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        onClick={() => dispatch({ type: "finished" })}
+        className="btn btn-ui"
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButton;
